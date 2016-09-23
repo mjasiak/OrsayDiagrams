44c311348e5c03e2bcaca3cb46afd49444014175
Michał Jasiak <michal.jasiak148@o2.pl>
2016-09-23 19:26:33 +0200

    Model Prepare

---
+++ src/DataContainer/Models/Petition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataContainer.Models
+{
+    public class Petition
+    {
+        public int ID { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime When_folded { get; set; }
+
+        public DateTime When_applied { get; set; }
+    }
+}
